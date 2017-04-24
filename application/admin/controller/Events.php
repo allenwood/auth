b97c6a73680dc8893dedc2a7a3e5480b11377ac5
Allen Wood <iandwho@163.com>
2017-04-24 11:52:43 +0800

    v8
    v8

--- application/admin/controller/Events.php
+++ application/admin/controller/Events.php
@@ -12,6 +12,8 @@ namespace app\admin\controller;
 class Events extends Admin
 {
     public function index(){
+        $lists=db('events')->where(['status'=>1])->order('istop desc,sort asc')->paginate(10);
+        $this->assign('lists',$lists);
         return view('index');
     }
 
@@ -30,7 +32,71 @@ class Events extends Admin
         return view('edit');
     }
 
+    /**
+     * publish
+     * @author Allen <wudi@wdcloud.cc>
+     */
     public function update(){
+        $data=input();
+        $data['update_time']=time();
+        if($data['istop']==1){
+            $data['sort']=0;
+        }
+        if(empty($data['id'])){
+            $data['create_time']=time();
+            $data['create_uid']=session('uid');
+            $data['update_uid']=session('uid');
+            $data['status']=1;
+            $re=db('events')->insert($data);
+            if($re!==false){
+                $this->success('操作成功：新闻已发布',url('admin/events/index'));
+            }else{
+                $this->error('操作失败');
+            }
+        }else{
+            $data['update_uid']=session('uid');
+            $re=db('events')->update($data);
+            if($re!==false){
+                $this->success('操作成功：新闻已更新',url('admin/events/index'));
+            }else{
+                $this->error('操作失败');
+            }
+        }
+
+
+    }
+
+    /**
+     * draft save
+     * @author Allen <wudi@wdcloud.cc>
+     */
+    /*public function save(){
+        $data=input();
+        $data['update_time']=time();
+        if(empty($data['id'])){
+            $data['create_time']=time();
+            $data['create_uid']=session('uid');
+            $data['update_uid']=session('uid');
+            $data['status']=2;
+            $re=db('events')->insert($data);
+            if($re!==false){
+                $this->success('操作成功：已保存到草稿箱');
+            }else{
+                $this->error('操作失败');
+            }
+        }else{
+            $data['update_uid']=session('uid');
+            $data['status']=2;
+            $re=db('events')->update($data);
+            if($re!==false){
+                $this->success('操作成功：新闻已经保存到草稿箱');
+            }else{
+                $this->error('操作失败');
+            }
+        }
+    }*/
+
+    public function del(){
 
     }
 }
